0891efcf7b301f4285fb3b5336e61c5b85396001
qazxsw1240 <qazxsw010528@gmail.com>
2024-07-02 15:13:58 +0900

    update PlayerNutritionEvent

--- src/main/java/com/github/test/entity/nutrition/PlayerNutritionAcquireEvent.java
+++ src/main/java/com/github/test/entity/nutrition/PlayerNutritionAcquireEvent.java
@@ -3,6 +3,10 @@ package com.github.test.entity.nutrition;
 import org.bukkit.entity.Player;
 
 public interface PlayerNutritionAcquireEvent {
+    public static PlayerNutritionAcquireEvent of(Player player, PlayerNutrition nutrition) {
+        return new PlayerNutritionAcquireEventImpl(player, nutrition);
+    }
+
     public abstract Player getPlayer();
 
     public abstract PlayerNutrition getNutrition();
--- src/main/java/com/github/test/entity/nutrition/PlayerNutritionConsumeEvent.java
+++ src/main/java/com/github/test/entity/nutrition/PlayerNutritionConsumeEvent.java
@@ -3,6 +3,10 @@ package com.github.test.entity.nutrition;
 import org.bukkit.entity.Player;
 
 public interface PlayerNutritionConsumeEvent {
+    public static PlayerNutritionConsumeEvent of(Player player, PlayerNutrition playerNutrition) {
+        return new PlayerNutritionConsumeEventImpl(player, playerNutrition);
+    }
+
     public abstract Player getPlayer();
 
     public abstract PlayerNutrition getNutrition();
